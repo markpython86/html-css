0e9875d7aa3529127f2600df62a1b139d603cf9e
Mark <mark.faraj86@gmail.com>
2021-01-10 15:51:12 -0600

    My First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("HelloAdam")
+
+var integer = 12;
+var float = 12.5;
+var string = '15';
+var array = ['1',2,3,4,5];
+console.log(array[1])
+// var 
+// console.log(number * 12)
+// console.log(number * 100)
+// console.log(number * 1000)
+// console.log(number + 12)
